c70878f9460efc9175ea537f46f276e80a66fef7
Alex Mojica <alexmojicamtz@gmail.com>
2020-07-09 11:34:53 +0200

    First commit

---
+++ src/components/context/github/githubContext.js
@@ -0,0 +1,6 @@
+//! 1. Inicializa un nuevo context
+import { createContext } from 'react';
+
+const githubContext = createContext();
+
+export default githubContext;
---
+++ src/components/pages/About.jsx
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+
+const About = () => {
+  return (
+    <Fragment>
+      <h1>Acerca de esta App</h1>
+      <p>Aplicación para buscar usuarios de Github.</p>
+      <p>Versión 1.0.0</p>
+    </Fragment>
+  );
+};
+
+export default About;
